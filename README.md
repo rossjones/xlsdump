136f320813c2ba9acbbedac7b720147d13459f02
Ross Jones <ross@servercode.co.uk>
2014-11-15 20:10:46 +0000

    Make sure the code does at least as much as the original

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # wip - xlsdump
 
-xsldump is a re-working on the xls2csv that is available within http://libxls.sourceforge.net which dumps all of the sheets in a spreadsheet to disk at a specific location.
+xsldump is a re-working of, **but substantially based on**, the xls2csv that is available within http://libxls.sourceforge.net which dumps all of the sheets in a spreadsheet to disk at a specific location.
 
 It is intended that xsldump will watch a folder for new .xls files, and generate new folders containing the .csv version of each sheet.
 
@@ -10,12 +10,12 @@ It is intended that xsldump will watch a folder for new .xls files, and generate
 # dependencies
 svn checkout svn://svn.code.sf.net/p/libxls/code/trunk libxls-code
 cd libxls-code
-./configure --prefix=/usr/local; 
-make; 
+./configure --prefix=/usr/local;
+make;
 sudo make install
 
 # app
 git clone git@github.com:rossjones/xlsdump.git
 cd xlsdump/
-make 
+make
 ```
--- xlsdump.c
+++ xlsdump.c
@@ -1,8 +1,17 @@
+/**
+*
+* Uses libxls and derived largely from work by Christophe Leitienne and
+* David Hoerl
+**/
 #include <ctype.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "libxls/xls.h"
+
+
+char stringSeparator = '"';
+char *fieldSeparator = ",";
 
 
 char *slugify(char *name) {
@@ -16,18 +25,29 @@ char *slugify(char *name) {
     return name;
 }
 
-int main(int argc, char*argv[]) {
+static void write_string(FILE *fd, const char *string) {
+    const char *str;
+
+    fprintf(fd, "%c", stringSeparator);
+    for (str = string; *str; str++) {
+        if (*str == stringSeparator) {
+            fprintf(fd, "%c%c", stringSeparator, stringSeparator);
+        } else if (*str == '\\') {
+            fprintf(fd, "\\\\");
+        } else {
+            fprintf(fd, "%c", *str);
+        }
+    }
+    fprintf(fd, "%c", stringSeparator);
+}
+
+int process(char *filename) {
     xlsWorkBook* pWorkbook;
     xlsWorkSheet* pSheet;
 
-    if ( argc != 2 ) {
-        fprintf(stderr, "usage: %s filename.xls\n", argv[0] );
-        return EXIT_FAILURE;
-    }
-
-    pWorkbook = xls_open(argv[1], "iso-8859-15//TRANSLIT");
+    pWorkbook = xls_open(filename, "iso-8859-15//TRANSLIT");
     if ( pWorkbook == NULL ) {
-        fprintf(stderr, "Couldn't find %s. Sorry\n", argv[1]);
+        fprintf(stderr, "Couldn't find %s.\n", filename);
         return EXIT_FAILURE;
     }
 
@@ -36,23 +56,70 @@ int main(int argc, char*argv[]) {
         char *name = (char *)pWorkbook->sheets.sheet[i].name;
         char *filename = malloc(strlen(name)+5); // null and .csv
         sprintf(filename, "%s.csv", slugify(name));
-        fprintf(stdout, "Sheet %d is called %s\n", i, filename);
+
+        FILE *fd = fopen(filename, "w");
 
         pSheet = xls_getWorkSheet(pWorkbook, i);
         xls_parseWorkSheet(pSheet);
 
         for (int cellRow = 0; cellRow <= pSheet->rows.lastrow; cellRow++) {
+            int isFirstCol = 1;
             for (int cellCol = 0; cellCol <= pSheet->rows.lastcol; cellCol++) {
                 xlsCell *cell = xls_cell(pSheet, cellRow, cellCol);
 
+                if ((!cell) || (cell->isHidden)) {
+                    continue;
+                }
+
+                if (!isFirstCol) {
+                    fprintf(fd, "%s", fieldSeparator);
+                } else {
+                    isFirstCol = 0;
+                }
+
+                if (cell->rowspan > 1) {
+                    fprintf(stderr, "Warning: %d rows spanned at col=%d row=%d: output will not match the Excel file.\n", cell->rowspan, cellCol+1, cellRow+1);
+                }
+
+                // display the value of the cell (either numeric or string)
+                if (cell->id == 0x27e || cell->id == 0x0BD || cell->id == 0x203) {
+                    fprintf(fd, "%.15g", cell->d);
+                } else if (cell->id == 0x06) {
+                    if (cell->l == 0) {
+                        fprintf(fd, "%.15g", cell->d);
+                    } else {
+                        /* Handle macros */
+                        if (!strcmp((char *)cell->str, "bool")) {
+                            write_string(fd, (int) cell->d ? "True" : "False");
+                        } else if (!strcmp((char *)cell->str, "error")) {
+                            write_string(fd, "*error*");
+                        } else {
+                            write_string(fd, (char *)cell->str);
+                        }
+                    }
+                } else if (cell->str != NULL) {
+                    write_string(fd, (char *)cell->str);
+                } else {
+                    write_string(fd, "");
+                }
 
             }
+            fprintf(fd, "\n");
         }
 
-
+        fclose(fd);
         free(filename);
     }
 
     xls_close(pWorkbook);
     return EXIT_SUCCESS;
+}
+
+int main(int argc, char*argv[]) {
+    if ( argc != 2 ) {
+        fprintf(stderr, "usage: %s filename.xls\n", argv[0] );
+        return EXIT_FAILURE;
+    }
+
+    return process(argv[1]);
 }
